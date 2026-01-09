9dd958a8bee63d3e8af1c6f3e02b1aedfc95a1da
EnlilSawa <enlil.sawa@hotmail.com>
2026-01-09 20:11:08 +0100

    Delete src directory

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import InvoiceForm from '@/components/InvoiceForm';
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-gray-100">
-      <InvoiceForm />
-    </main>
-  );
-}
